48fa4309577197297f186c2bd052919e5820aa7a
Cody Winton <cody.t.winton@gmail.com>
2018-02-22 11:50:37 -0600

    Add project files.

---
+++ AspNetCoreKata/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreKata.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
